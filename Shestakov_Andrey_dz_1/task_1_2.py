a14d08d0e35f910b51037a0eb30ce460febc3f9e
AndreyShestakov <johnniegontar@gmail.com>
2022-02-22 17:39:17 +0300

    change task_1_2.py

--- Shestakov_Andrey_dz_1/task_1_2.py
+++ Shestakov_Andrey_dz_1/task_1_2.py
@@ -1,36 +1,36 @@
 def generate_cube_list(start: int, end: int) -> list:
     my_list = []
-    for idx in range(start, end + 1):
-        cube = f'{idx ** 3}'
-        sum_num = 0
-        for num in cube:
-            sum_num += int(num)
-
-        if sum_num % 7 == 0:
-            my_list.append(idx)
-
+    for n in range(start, end + 1, 2):
+        my_list.append(n ** 3)
     return my_list
+
+
+def check_by_7(n: int) -> int:
+    sum_n = 0
+    for i in str(n):
+        sum_n += int(i)
+    if sum_n % 7 == 0:
+        return n
+    else:
+        return 0
 
 
 def sum_list_1(dataset: list) -> int:
     all_sum = 0
     for num in dataset:
-        all_sum += num
+        all_sum += check_by_7(num)
     return all_sum
 
 
 def sum_list_2(dataset: list) -> int:
     all_sum = 0
-    cur_num_sum = 0
     for num in dataset:
-        for cur_num in str(num + 17):
-            cur_num_sum += int(cur_num)
-        if cur_num_sum % 7 == 0:
-            all_sum += num
+        all_sum += check_by_7(num + 17)
     return all_sum
 
 
 my_list = generate_cube_list(1, 1000)
+
 result_1 = sum_list_1(my_list)
 print(result_1)
 result_2 = sum_list_2(my_list)
